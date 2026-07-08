31e30aae6591f13f2c654016bcefa42b9ca01bc5
Sebastian Navarrete <juanse8014.jsnp@gmail.com>
2026-07-07 20:48:23 -0500

    Working with macros and Jinja

---
+++ macros/test.sql
@@ -0,0 +1,27 @@
+{% macro random_macro() %}
+
+{# If you don't put info=True in the function, it will be printed in the dbt.log file, but not in console. #}
+{{ log('Hello', info=True) }} 
+
+{% set query %}
+select
+distinct token_address
+from {{ ref('stg_token_transfers') }}
+limit 10
+{% endset %}
+
+{# To execute the query #}
+
+{% if execute%}
+{% set result = run_query(query) %}
+{% set result_list = result.columns[0].values() %}
+{% else %}
+{% set result_list = [] %}
+{% endif %}
+
+{{ log(result_list, info=True) }} 
+
+{# We also can return the output of the macro #}
+{{ return(result_list) }}
+
+{% endmacro %}
